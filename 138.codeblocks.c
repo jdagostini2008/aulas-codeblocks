3900a2a139c450ac5013430f6d880415572733e7
Julia D'Agostini <jdagostini2008@gmail.com>
2023-01-28 11:09:46 -0300

    aulas codeblocks

---
+++ 138.codeblocks.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+
+ int idade
+  double salario, altura;
+  char genero;
+  char nome[50];
+
+   idade = 20;
+   salario = 5800;
+   altura = 1.63;
+   genero = 'f';
+
+   printf()
+return 0;
+}
+
