e343b23c811c6aee4447db547b630e9dd51078d4
Julia D'Agostini <jdagostini2008@gmail.com>
2023-02-24 15:22:39 -0300

    aula codeblocks

---
+++ 146estruturacondicional.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+int hora;
+printf ("Digite uma hora do dia: ");
+scanf("%d", &hora);
+if (hora < 12)
+{
+       printf("Bomdia!\n");
+
+}
+  return 0;
+
